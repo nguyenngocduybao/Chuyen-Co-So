5eb3576cb730c2f434e7912163a2ef0ed8ef3e91
nndb05123 <duybao05123@gmail.com>
2018-01-29 03:08:13 +0700

    Design

---
+++ ChuyenCoSo/Form1.Designer.cs
@@ -0,0 +1,227 @@
+﻿namespace ChuyenCoSo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label_Nhap = new System.Windows.Forms.Label();
+            this.textbox_Nhap = new System.Windows.Forms.TextBox();
+            this.decTobin = new System.Windows.Forms.Button();
+            this.decTooct = new System.Windows.Forms.Button();
+            this.decTohex = new System.Windows.Forms.Button();
+            this.binTodec = new System.Windows.Forms.Button();
+            this.binTooct = new System.Windows.Forms.Button();
+            this.binTohex = new System.Windows.Forms.Button();
+            this.octTodec = new System.Windows.Forms.Button();
+            this.octTobin = new System.Windows.Forms.Button();
+            this.octTohex = new System.Windows.Forms.Button();
+            this.hexTodec = new System.Windows.Forms.Button();
+            this.label_Xuat = new System.Windows.Forms.Label();
+            this.hexTobin = new System.Windows.Forms.Button();
+            this.hexTooct = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label_Nhap
+            // 
+            this.label_Nhap.AutoSize = true;
+            this.label_Nhap.Location = new System.Drawing.Point(217, 61);
+            this.label_Nhap.Name = "label_Nhap";
+            this.label_Nhap.Size = new System.Drawing.Size(51, 20);
+            this.label_Nhap.TabIndex = 0;
+            this.label_Nhap.Text = "label1";
+            // 
+            // textbox_Nhap
+            // 
+            this.textbox_Nhap.Location = new System.Drawing.Point(221, 111);
+            this.textbox_Nhap.Name = "textbox_Nhap";
+            this.textbox_Nhap.Size = new System.Drawing.Size(239, 26);
+            this.textbox_Nhap.TabIndex = 1;
+            // 
+            // decTobin
+            // 
+            this.decTobin.Location = new System.Drawing.Point(63, 170);
+            this.decTobin.Name = "decTobin";
+            this.decTobin.Size = new System.Drawing.Size(98, 36);
+            this.decTobin.TabIndex = 2;
+            this.decTobin.Text = "Dec->Bin";
+            this.decTobin.UseVisualStyleBackColor = true;
+            // 
+            // decTooct
+            // 
+            this.decTooct.Location = new System.Drawing.Point(63, 228);
+            this.decTooct.Name = "decTooct";
+            this.decTooct.Size = new System.Drawing.Size(98, 35);
+            this.decTooct.TabIndex = 3;
+            this.decTooct.Text = "Dec->Oct";
+            this.decTooct.UseVisualStyleBackColor = true;
+            // 
+            // decTohex
+            // 
+            this.decTohex.Location = new System.Drawing.Point(63, 285);
+            this.decTohex.Name = "decTohex";
+            this.decTohex.Size = new System.Drawing.Size(98, 33);
+            this.decTohex.TabIndex = 4;
+            this.decTohex.Text = "Dec->Hex";
+            this.decTohex.UseVisualStyleBackColor = true;
+            // 
+            // binTodec
+            // 
+            this.binTodec.Location = new System.Drawing.Point(221, 170);
+            this.binTodec.Name = "binTodec";
+            this.binTodec.Size = new System.Drawing.Size(124, 36);
+            this.binTodec.TabIndex = 5;
+            this.binTodec.Text = "Bin->Dec";
+            this.binTodec.UseVisualStyleBackColor = true;
+            // 
+            // binTooct
+            // 
+            this.binTooct.Location = new System.Drawing.Point(221, 228);
+            this.binTooct.Name = "binTooct";
+            this.binTooct.Size = new System.Drawing.Size(124, 35);
+            this.binTooct.TabIndex = 6;
+            this.binTooct.Text = "Bin->Oct";
+            this.binTooct.UseVisualStyleBackColor = true;
+            // 
+            // binTohex
+            // 
+            this.binTohex.Location = new System.Drawing.Point(221, 285);
+            this.binTohex.Name = "binTohex";
+            this.binTohex.Size = new System.Drawing.Size(124, 33);
+            this.binTohex.TabIndex = 7;
+            this.binTohex.Text = "Bin->Hex";
+            this.binTohex.UseVisualStyleBackColor = true;
+            // 
+            // octTodec
+            // 
+            this.octTodec.Location = new System.Drawing.Point(393, 170);
+            this.octTodec.Name = "octTodec";
+            this.octTodec.Size = new System.Drawing.Size(110, 36);
+            this.octTodec.TabIndex = 8;
+            this.octTodec.Text = "Oct->Dec";
+            this.octTodec.UseVisualStyleBackColor = true;
+            // 
+            // octTobin
+            // 
+            this.octTobin.Location = new System.Drawing.Point(393, 228);
+            this.octTobin.Name = "octTobin";
+            this.octTobin.Size = new System.Drawing.Size(110, 35);
+            this.octTobin.TabIndex = 9;
+            this.octTobin.Text = "Oct->Bin";
+            this.octTobin.UseVisualStyleBackColor = true;
+            // 
+            // octTohex
+            // 
+            this.octTohex.Location = new System.Drawing.Point(393, 285);
+            this.octTohex.Name = "octTohex";
+            this.octTohex.Size = new System.Drawing.Size(110, 33);
+            this.octTohex.TabIndex = 10;
+            this.octTohex.Text = "Oct->Hex";
+            this.octTohex.UseVisualStyleBackColor = true;
+            // 
+            // hexTodec
+            // 
+            this.hexTodec.Location = new System.Drawing.Point(550, 170);
+            this.hexTodec.Name = "hexTodec";
+            this.hexTodec.Size = new System.Drawing.Size(94, 36);
+            this.hexTodec.TabIndex = 11;
+            this.hexTodec.Text = "Hex->Dec";
+            this.hexTodec.UseVisualStyleBackColor = true;
+            // 
+            // label_Xuat
+            // 
+            this.label_Xuat.AutoSize = true;
+            this.label_Xuat.Location = new System.Drawing.Point(221, 363);
+            this.label_Xuat.Name = "label_Xuat";
+            this.label_Xuat.Size = new System.Drawing.Size(51, 20);
+            this.label_Xuat.TabIndex = 12;
+            this.label_Xuat.Text = "label1";
+            // 
+            // hexTobin
+            // 
+            this.hexTobin.Location = new System.Drawing.Point(550, 228);
+            this.hexTobin.Name = "hexTobin";
+            this.hexTobin.Size = new System.Drawing.Size(94, 35);
+            this.hexTobin.TabIndex = 13;
+            this.hexTobin.Text = "Hex->Bin";
+            this.hexTobin.UseVisualStyleBackColor = true;
+            // 
+            // hexTooct
+            // 
+            this.hexTooct.Location = new System.Drawing.Point(550, 285);
+            this.hexTooct.Name = "hexTooct";
+            this.hexTooct.Size = new System.Drawing.Size(94, 33);
+            this.hexTooct.TabIndex = 14;
+            this.hexTooct.Text = "Hex->Oct";
+            this.hexTooct.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(684, 438);
+            this.Controls.Add(this.hexTooct);
+            this.Controls.Add(this.hexTobin);
+            this.Controls.Add(this.label_Xuat);
+            this.Controls.Add(this.hexTodec);
+            this.Controls.Add(this.octTohex);
+            this.Controls.Add(this.octTobin);
+            this.Controls.Add(this.octTodec);
+            this.Controls.Add(this.binTohex);
+            this.Controls.Add(this.binTooct);
+            this.Controls.Add(this.binTodec);
+            this.Controls.Add(this.decTohex);
+            this.Controls.Add(this.decTooct);
+            this.Controls.Add(this.decTobin);
+            this.Controls.Add(this.textbox_Nhap);
+            this.Controls.Add(this.label_Nhap);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label_Nhap;
+        private System.Windows.Forms.TextBox textbox_Nhap;
+        private System.Windows.Forms.Button decTobin;
+        private System.Windows.Forms.Button decTooct;
+        private System.Windows.Forms.Button decTohex;
+        private System.Windows.Forms.Button binTodec;
+        private System.Windows.Forms.Button binTooct;
+        private System.Windows.Forms.Button binTohex;
+        private System.Windows.Forms.Button octTodec;
+        private System.Windows.Forms.Button octTobin;
+        private System.Windows.Forms.Button octTohex;
+        private System.Windows.Forms.Button hexTodec;
+        private System.Windows.Forms.Label label_Xuat;
+        private System.Windows.Forms.Button hexTobin;
+        private System.Windows.Forms.Button hexTooct;
+    }
+}
+
