ae559374a96d31b6c86481366e622d73bb4458c3
bachsoda326 <sadok125a@gmail.com>
2018-02-02 08:27:30 +0700

    Code

--- ChuyenCoSo/Form1.Designer.cs
+++ ChuyenCoSo/Form1.Designer.cs
@@ -29,7 +29,7 @@
         private void InitializeComponent()
         {
             this.label_Nhap = new System.Windows.Forms.Label();
-            this.textbox_Nhap = new System.Windows.Forms.TextBox();
+            this.tb_Nhap = new System.Windows.Forms.TextBox();
             this.decTobin = new System.Windows.Forms.Button();
             this.decTooct = new System.Windows.Forms.Button();
             this.decTohex = new System.Windows.Forms.Button();
@@ -43,146 +43,182 @@
             this.label_Xuat = new System.Windows.Forms.Label();
             this.hexTobin = new System.Windows.Forms.Button();
             this.hexTooct = new System.Windows.Forms.Button();
+            this.tb_Xuat = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // label_Nhap
             // 
             this.label_Nhap.AutoSize = true;
-            this.label_Nhap.Location = new System.Drawing.Point(217, 61);
+            this.label_Nhap.Location = new System.Drawing.Point(102, 48);
+            this.label_Nhap.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label_Nhap.Name = "label_Nhap";
-            this.label_Nhap.Size = new System.Drawing.Size(51, 20);
+            this.label_Nhap.Size = new System.Drawing.Size(33, 13);
             this.label_Nhap.TabIndex = 0;
-            this.label_Nhap.Text = "label1";
+            this.label_Nhap.Text = "Nhap";
             // 
-            // textbox_Nhap
+            // tb_Nhap
             // 
-            this.textbox_Nhap.Location = new System.Drawing.Point(221, 111);
-            this.textbox_Nhap.Name = "textbox_Nhap";
-            this.textbox_Nhap.Size = new System.Drawing.Size(239, 26);
-            this.textbox_Nhap.TabIndex = 1;
+            this.tb_Nhap.Location = new System.Drawing.Point(163, 45);
+            this.tb_Nhap.Margin = new System.Windows.Forms.Padding(2);
+            this.tb_Nhap.Name = "tb_Nhap";
+            this.tb_Nhap.Size = new System.Drawing.Size(161, 20);
+            this.tb_Nhap.TabIndex = 1;
             // 
             // decTobin
             // 
-            this.decTobin.Location = new System.Drawing.Point(63, 170);
+            this.decTobin.Location = new System.Drawing.Point(42, 95);
+            this.decTobin.Margin = new System.Windows.Forms.Padding(2);
             this.decTobin.Name = "decTobin";
-            this.decTobin.Size = new System.Drawing.Size(98, 36);
+            this.decTobin.Size = new System.Drawing.Size(65, 23);
             this.decTobin.TabIndex = 2;
             this.decTobin.Text = "Dec->Bin";
             this.decTobin.UseVisualStyleBackColor = true;
+            this.decTobin.Click += new System.EventHandler(this.decTobin_Click);
             // 
             // decTooct
             // 
-            this.decTooct.Location = new System.Drawing.Point(63, 228);
+            this.decTooct.Location = new System.Drawing.Point(42, 135);
+            this.decTooct.Margin = new System.Windows.Forms.Padding(2);
             this.decTooct.Name = "decTooct";
-            this.decTooct.Size = new System.Drawing.Size(98, 35);
+            this.decTooct.Size = new System.Drawing.Size(65, 23);
             this.decTooct.TabIndex = 3;
             this.decTooct.Text = "Dec->Oct";
             this.decTooct.UseVisualStyleBackColor = true;
+            this.decTooct.Click += new System.EventHandler(this.decTooct_Click);
             // 
             // decTohex
             // 
-            this.decTohex.Location = new System.Drawing.Point(63, 285);
+            this.decTohex.Location = new System.Drawing.Point(42, 174);
+            this.decTohex.Margin = new System.Windows.Forms.Padding(2);
             this.decTohex.Name = "decTohex";
-            this.decTohex.Size = new System.Drawing.Size(98, 33);
+            this.decTohex.Size = new System.Drawing.Size(65, 21);
             this.decTohex.TabIndex = 4;
             this.decTohex.Text = "Dec->Hex";
             this.decTohex.UseVisualStyleBackColor = true;
+            this.decTohex.Click += new System.EventHandler(this.decTohex_Click);
             // 
             // binTodec
             // 
-            this.binTodec.Location = new System.Drawing.Point(221, 170);
+            this.binTodec.Location = new System.Drawing.Point(147, 95);
+            this.binTodec.Margin = new System.Windows.Forms.Padding(2);
             this.binTodec.Name = "binTodec";
-            this.binTodec.Size = new System.Drawing.Size(124, 36);
+            this.binTodec.Size = new System.Drawing.Size(83, 23);
             this.binTodec.TabIndex = 5;
             this.binTodec.Text = "Bin->Dec";
             this.binTodec.UseVisualStyleBackColor = true;
+            this.binTodec.Click += new System.EventHandler(this.binTodec_Click);
             // 
             // binTooct
             // 
-            this.binTooct.Location = new System.Drawing.Point(221, 228);
+            this.binTooct.Location = new System.Drawing.Point(147, 135);
+            this.binTooct.Margin = new System.Windows.Forms.Padding(2);
             this.binTooct.Name = "binTooct";
-            this.binTooct.Size = new System.Drawing.Size(124, 35);
+            this.binTooct.Size = new System.Drawing.Size(83, 23);
             this.binTooct.TabIndex = 6;
             this.binTooct.Text = "Bin->Oct";
             this.binTooct.UseVisualStyleBackColor = true;
+            this.binTooct.Click += new System.EventHandler(this.binTooct_Click);
             // 
             // binTohex
             // 
-            this.binTohex.Location = new System.Drawing.Point(221, 285);
+            this.binTohex.Location = new System.Drawing.Point(147, 174);
+            this.binTohex.Margin = new System.Windows.Forms.Padding(2);
             this.binTohex.Name = "binTohex";
-            this.binTohex.Size = new System.Drawing.Size(124, 33);
+            this.binTohex.Size = new System.Drawing.Size(83, 21);
             this.binTohex.TabIndex = 7;
             this.binTohex.Text = "Bin->Hex";
             this.binTohex.UseVisualStyleBackColor = true;
+            this.binTohex.Click += new System.EventHandler(this.binTohex_Click);
             // 
             // octTodec
             // 
-            this.octTodec.Location = new System.Drawing.Point(393, 170);
+            this.octTodec.Location = new System.Drawing.Point(262, 95);
+            this.octTodec.Margin = new System.Windows.Forms.Padding(2);
             this.octTodec.Name = "octTodec";
-            this.octTodec.Size = new System.Drawing.Size(110, 36);
+            this.octTodec.Size = new System.Drawing.Size(73, 23);
             this.octTodec.TabIndex = 8;
             this.octTodec.Text = "Oct->Dec";
             this.octTodec.UseVisualStyleBackColor = true;
+            this.octTodec.Click += new System.EventHandler(this.octTodec_Click);
             // 
             // octTobin
             // 
-            this.octTobin.Location = new System.Drawing.Point(393, 228);
+            this.octTobin.Location = new System.Drawing.Point(262, 135);
+            this.octTobin.Margin = new System.Windows.Forms.Padding(2);
             this.octTobin.Name = "octTobin";
-            this.octTobin.Size = new System.Drawing.Size(110, 35);
+            this.octTobin.Size = new System.Drawing.Size(73, 23);
             this.octTobin.TabIndex = 9;
             this.octTobin.Text = "Oct->Bin";
             this.octTobin.UseVisualStyleBackColor = true;
+            this.octTobin.Click += new System.EventHandler(this.octTobin_Click);
             // 
             // octTohex
             // 
-            this.octTohex.Location = new System.Drawing.Point(393, 285);
+            this.octTohex.Location = new System.Drawing.Point(262, 174);
+            this.octTohex.Margin = new System.Windows.Forms.Padding(2);
             this.octTohex.Name = "octTohex";
-            this.octTohex.Size = new System.Drawing.Size(110, 33);
+            this.octTohex.Size = new System.Drawing.Size(73, 21);
             this.octTohex.TabIndex = 10;
             this.octTohex.Text = "Oct->Hex";
             this.octTohex.UseVisualStyleBackColor = true;
+            this.octTohex.Click += new System.EventHandler(this.octTohex_Click);
             // 
             // hexTodec
             // 
-            this.hexTodec.Location = new System.Drawing.Point(550, 170);
+            this.hexTodec.Location = new System.Drawing.Point(367, 95);
+            this.hexTodec.Margin = new System.Windows.Forms.Padding(2);
             this.hexTodec.Name = "hexTodec";
-            this.hexTodec.Size = new System.Drawing.Size(94, 36);
+            this.hexTodec.Size = new System.Drawing.Size(63, 23);
             this.hexTodec.TabIndex = 11;
             this.hexTodec.Text = "Hex->Dec";
             this.hexTodec.UseVisualStyleBackColor = true;
+            this.hexTodec.Click += new System.EventHandler(this.hexTodec_Click);
             // 
             // label_Xuat
             // 
             this.label_Xuat.AutoSize = true;
-            this.label_Xuat.Location = new System.Drawing.Point(221, 363);
+            this.label_Xuat.Location = new System.Drawing.Point(102, 225);
+            this.label_Xuat.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label_Xuat.Name = "label_Xuat";
-            this.label_Xuat.Size = new System.Drawing.Size(51, 20);
+            this.label_Xuat.Size = new System.Drawing.Size(44, 13);
             this.label_Xuat.TabIndex = 12;
-            this.label_Xuat.Text = "label1";
+            this.label_Xuat.Text = "Ket qua";
             // 
             // hexTobin
             // 
-            this.hexTobin.Location = new System.Drawing.Point(550, 228);
+            this.hexTobin.Location = new System.Drawing.Point(367, 135);
+            this.hexTobin.Margin = new System.Windows.Forms.Padding(2);
             this.hexTobin.Name = "hexTobin";
-            this.hexTobin.Size = new System.Drawing.Size(94, 35);
+            this.hexTobin.Size = new System.Drawing.Size(63, 23);
             this.hexTobin.TabIndex = 13;
             this.hexTobin.Text = "Hex->Bin";
             this.hexTobin.UseVisualStyleBackColor = true;
+            this.hexTobin.Click += new System.EventHandler(this.hexTobin_Click);
             // 
             // hexTooct
             // 
-            this.hexTooct.Location = new System.Drawing.Point(550, 285);
+            this.hexTooct.Location = new System.Drawing.Point(367, 174);
+            this.hexTooct.Margin = new System.Windows.Forms.Padding(2);
             this.hexTooct.Name = "hexTooct";
-            this.hexTooct.Size = new System.Drawing.Size(94, 33);
+            this.hexTooct.Size = new System.Drawing.Size(63, 21);
             this.hexTooct.TabIndex = 14;
             this.hexTooct.Text = "Hex->Oct";
             this.hexTooct.UseVisualStyleBackColor = true;
+            this.hexTooct.Click += new System.EventHandler(this.hexTooct_Click);
+            // 
+            // tb_Xuat
+            // 
+            this.tb_Xuat.Location = new System.Drawing.Point(163, 222);
+            this.tb_Xuat.Name = "tb_Xuat";
+            this.tb_Xuat.Size = new System.Drawing.Size(161, 20);
+            this.tb_Xuat.TabIndex = 15;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(684, 438);
+            this.ClientSize = new System.Drawing.Size(456, 285);
+            this.Controls.Add(this.tb_Xuat);
             this.Controls.Add(this.hexTooct);
             this.Controls.Add(this.hexTobin);
             this.Controls.Add(this.label_Xuat);
@@ -196,10 +232,11 @@
             this.Controls.Add(this.decTohex);
             this.Controls.Add(this.decTooct);
             this.Controls.Add(this.decTobin);
-            this.Controls.Add(this.textbox_Nhap);
+            this.Controls.Add(this.tb_Nhap);
             this.Controls.Add(this.label_Nhap);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "ChuyenCoSo";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -208,7 +245,7 @@
         #endregion
 
         private System.Windows.Forms.Label label_Nhap;
-        private System.Windows.Forms.TextBox textbox_Nhap;
+        private System.Windows.Forms.TextBox tb_Nhap;
         private System.Windows.Forms.Button decTobin;
         private System.Windows.Forms.Button decTooct;
         private System.Windows.Forms.Button decTohex;
@@ -222,6 +259,7 @@
         private System.Windows.Forms.Label label_Xuat;
         private System.Windows.Forms.Button hexTobin;
         private System.Windows.Forms.Button hexTooct;
+        private System.Windows.Forms.TextBox tb_Xuat;
     }
 }
 
--- ChuyenCoSo/Form1.cs
+++ ChuyenCoSo/Form1.cs
@@ -17,6 +17,302 @@ namespace ChuyenCoSo
             InitializeComponent();
         }
 
+        int Count(int N)
+        {
+            int t = 0;
+            while (N > 0)
+            {
+                t++;
+                N = N / 10;
+            }
+            return t;
+        }
 
+        void Result (int x)
+        {
+            tb_Xuat.Text = x.ToString();
+        }
+
+        int Checkc (char a)
+        {
+            int b;
+
+            switch (a)
+            {
+                case 'A':
+                    b = 10;
+                    break;
+                case 'B':
+                    b = 11;
+                    break;
+                case 'C' :
+                    b = 12;
+                    break;
+                case 'D':
+                    b = 13;
+                    break;
+                case 'E':
+                    b = 14;
+                    break;
+                case 'F':
+                    b = 15;
+                    break;
+                default:
+                    b = (int)a - 48;
+                    break;
+            }
+
+            return b;
+        }
+
+        string Checki (int x)
+        {
+            string a;
+
+            switch (x)
+            {
+                case 10:
+                    a = "A";
+                    break;
+                case 11:
+                    a = "B";
+                    break;
+                case 12:
+                    a = "C";
+                    break;
+                case 13:
+                    a = "D";
+                    break;
+                case 14:
+                    a = "E";
+                    break;
+                case 15:
+                    a = "F";
+                    break;
+                default:
+                    a = x.ToString();
+                    break;
+            }
+
+            return a;
+        }
+
+        string Reverse(string a)
+        {
+            char[] c = a.ToCharArray();
+            Array.Reverse(c);
+            return new string(c);
+        }
+
+        int bindec()
+        {
+            int a = Convert.ToInt32(tb_Nhap.Text);
+            int x = 0, y = 0, k = Count(a);
+
+            for (int i = k; i >= 0; i--)
+            {
+                y = (int)(Math.Pow(2, i)) * (int)(a / Math.Pow(10, i));
+                x += y;
+                a = a % (int)(Math.Pow(10, i));
+            }
+
+            return x;
+        }
+
+        private void binTodec_Click(object sender, EventArgs e)
+        {
+            int x = bindec();
+
+            Result(x);
+        }
+
+        int octdec()
+        {
+            int a = Convert.ToInt32(tb_Nhap.Text);
+            int x = 0, y = 0, k = Count(a);
+
+            for (int i = k; i >= 0; i--)
+            {
+                y = (int)(Math.Pow(8, i)) * (int)(a / Math.Pow(10, i));
+                x += y;
+                a = a % (int)(Math.Pow(10, i));
+            }
+
+            return x;
+        }
+
+        private void octTodec_Click(object sender, EventArgs e)
+        {
+            int x = octdec();
+
+            Result(x);
+        }
+
+        int hexdec()
+        {
+            string a = tb_Nhap.Text;
+            int x = 0, y = 0, k = a.Length;
+
+            for (int i = k - 1; i >= 0; i--)
+            {
+                y = (int)(Math.Pow(16, k - i - 1)) * Checkc(a[i]);
+                x += y;
+            }
+
+            return x;
+        }
+
+        private void hexTodec_Click(object sender, EventArgs e)
+        {
+            int x = hexdec();
+
+            Result(x);
+        }
+                
+        private void decTobin_Click(object sender, EventArgs e)
+        {
+            int a = Convert.ToInt32(tb_Nhap.Text), b;
+            string c = "";            
+
+            do
+            {
+                b = (int)(a % 2);
+                c += b.ToString();                
+                a = a / 2;
+            } while (a != 0);
+            c = Reverse(c);
+
+            tb_Xuat.Text = c;
+        }
+                
+        private void decTooct_Click(object sender, EventArgs e)
+        {
+            int a = Convert.ToInt32(tb_Nhap.Text), b;
+            string c = "";
+
+            do
+            {
+                b = (int)(a % 8);
+                c += b.ToString();
+                a = a / 8;
+            } while (a != 0);
+            c = Reverse(c);
+
+            tb_Xuat.Text = c;
+        }
+
+        private void decTohex_Click(object sender, EventArgs e)
+        {
+            int a = Convert.ToInt32(tb_Nhap.Text), b;
+            string c = "", d;
+
+            do
+            {
+                b = (int)(a % 16);
+                d = Checki(b);
+                c += d;
+                a = a / 16;
+            } while (a != 0);
+            c = Reverse(c);
+
+            tb_Xuat.Text = c;
+        }
+
+        private void octTobin_Click(object sender, EventArgs e)
+        {
+            int a = octdec(), b;
+            string c = "";
+
+            do
+            {
+                b = (int)(a % 2);
+                c += b.ToString();
+                a = a / 2;
+            } while (a != 0);
+            c = Reverse(c);
+
+            tb_Xuat.Text = c;
+        }
+
+        private void octTohex_Click(object sender, EventArgs e)
+        {
+            int a = octdec(), b;
+            string c = "";
+
+            do
+            {
+                b = (int)(a % 16);
+                c += b.ToString();
+                a = a / 16;
+            } while (a != 0);
+            c = Reverse(c);
+
+            tb_Xuat.Text = c;
+        }
+
+        private void hexTobin_Click(object sender, EventArgs e)
+        {
+            int a = hexdec(), b;
+            string c = "";
+
+            do
+            {
+                b = (int)(a % 2);
+                c += b.ToString();
+                a = a / 2;
+            } while (a != 0);
+            c = Reverse(c);
+
+            tb_Xuat.Text = c;
+        }
+
+        private void hexTooct_Click(object sender, EventArgs e)
+        {
+            int a = hexdec(), b;
+            string c = "";
+
+            do
+            {
+                b = (int)(a % 8);
+                c += b.ToString();
+                a = a / 8;
+            } while (a != 0);
+            c = Reverse(c);
+
+            tb_Xuat.Text = c;
+        }
+
+        private void binTooct_Click(object sender, EventArgs e)
+        {
+            int a = bindec(), b;
+            string c = "";
+
+            do
+            {
+                b = (int)(a % 8);
+                c += b.ToString();
+                a = a / 8;
+            } while (a != 0);
+            c = Reverse(c);
+
+            tb_Xuat.Text = c;
+        }
+
+        private void binTohex_Click(object sender, EventArgs e)
+        {
+            int a = bindec(), b;            
+            string c = "", d;
+
+            do
+            {
+                b = (int)(a % 16);
+                d = Checki(b);
+                c += d;
+                a = a / 16;
+            } while (a != 0);
+            c = Reverse(c);
+
+            tb_Xuat.Text = c;
+        }
     }
 }
